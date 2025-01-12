1e08b4b8555d495a3fda6aa9dc593ada3b76ea4c
radhamusicbot <radhamusicbot@gmail.com>
2025-01-12 10:31:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -90,7 +90,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/fcawaj.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/gcauas.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/fcawaj.jpg"
